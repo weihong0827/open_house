4dcb63f0727592489114fc6eebe91d3c843f1f64
weihong0827 <48542562+weihong0827@users.noreply.github.com>
2021-02-18 13:53:23 +0800

    second commit

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -8,7 +8,7 @@ function Background(props) {
     return (
         <div className={"w-full h-screen-70"}>
             <img src={pic} className={"relative w-full h-screen-70 object-cover"}/>
-            <header className={"relative w-screen h-auto -top-2/3 font-medium font-sans text-6xl text-center text-white"}>FIFTHROW SHOWCASE</header>
+            <header className={"relative w-full h-auto -top-2/3 font-medium font-sans text-6xl text-center text-white"}>FIFTHROW SHOWCASE</header>
         </div>
     );
 }
