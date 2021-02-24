739ee9af8d7097329196099eee66a59d7dd76bd4
weihong0827 <48542562+weihong0827@users.noreply.github.com>
2021-02-24 11:35:17 +0800

    update footer info

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ig from "../Images/Icons/Instagram-Logo.wine-2.png";
 import fb from "../Images/Icons/fb.png";
 import telegram from "../Images/Icons/telegram-icon.png";
+import mail_icon from "../Images/Icons/aiga_mail_bg-512.png";
 
 function Footer(props) {
     return (
@@ -19,21 +20,22 @@ function Footer(props) {
             <div className={"h-3/5 w-1/4 flex flex-col"}>
                 <h6 className={"text-white font-bold text-sm md:text-base"}>Follow Us</h6>
                 <div className={"w-full inline-block mt-2 md:mt-6"}>
-                    <a href={'#'} className={"mr-2 md:mr-4"}>
+                    <a href={'https://t.me/SUTDROOTAnnouncements'} className={"mr-2 md:mr-4"}>
                         <img src={telegram} className={'h-6 w-6 inline-block md:h-8 md:w-8 lg:h-10 lg:w-10'}/>
                     </a>
 
-                    <a href={'#'} className={"mr-2 md:mr-4"}>
+                    <a href={'https://www.facebook.com/groups/522995557746987/'} className={"mr-2 md:mr-4"}>
                         <img src={fb} className={'h-6 w-6 inline-block md:h-8 md:w-8 lg:h-10 lg:w-10'}/>
                     </a>
-                    <a href={'#'} >
-
+                    <a href={'https://www.instagram.com/sutdlife'} target={'_blank'}>
                         <img src={ig} className={'h-6 w-6 inline-block md:h-8 md:w-8 lg:h-10 lg:w-10'}/>
                     </a>
                 </div>
             </div>
             <div className={"h-3/5 w-1/4 flex flex-col"}>
                 <h6 className={"text-white font-bold text-sm md:text-base"}>Contact Us</h6>
+
+                <a className={"text-gray-200 no-underline m-0 hover:text-gray-200 text-xs md:text-base overflow-x-scroll"} href = "mailto:communications@studentgov.sutd.edu.sg">communications@studentgov.sutd.edu.sg</a>
             </div>
         </div>
     );
